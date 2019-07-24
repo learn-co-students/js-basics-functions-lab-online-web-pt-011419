55dfa841ae9b14ad1da02f3c8a10d21d3435e8ea
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-24 18:10:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  let distance = '';
+
+  if(block > 42) {
+    distance = block - 42;
+  } else {
+    distance = 42 - block;
+  }
+
+  return distance;
+
+}
+
+
+function distanceFromHqInFeet (block) {
+  let distance;
+
+  distance = distanceFromHqInBlocks(block) * 264;
+
+  return distance;
+
+}
+
+function distanceTravelledInFeet(start, end) {
+  let distance = '';
+
+  if(start > end) {
+    distance = (start - end) * 264;
+  } else {
+    distance = (end - start) * 264;
+  }
+
+  return distance;
+
+}
+
+function calculatesFarePrice(start, end) {
+  let distance = distanceTravelledInFeet(start, end);
+  let price;
+
+   if (distance  > 2500) {
+      price = 'cannot travel that far';
+   } else if (distance < 2500 && distance > 2000) {
+     price = 25;
+   } else if (distance > 400 && distance < 2000) {
+      price = (distance - 400) * .02
+   } else {
+     price = 0;
+   }
+  return price;
+}
