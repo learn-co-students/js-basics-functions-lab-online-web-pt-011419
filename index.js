d53519111e89966d16f956ec7225faecd8e77dd7
NYHolzer <nissan.holzer@gmail.com>
2019-07-28 21:10:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(numOfBlock) {
+    return Math.abs(numOfBlock - 42)
+}
+
+function distanceFromHqInFeet(numOfBlock) {
+    return distanceFromHqInBlocks(numOfBlock) * 264
+}
+
+function distanceTravelledInFeet(numOfBlock1, numOfBlock2) {
+    return Math.abs(numOfBlock2 - numOfBlock1) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare;
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance <= 2000 && distance > 400) {
+        fare = (distance - 400) * 0.02
+    } else if (distance > 2000 && distance < 2500) {
+        fare = 25
+    } else {
+        fare = 'cannot travel that far'
+    }
+    return fare
+}
